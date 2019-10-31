c25ed00da173ea9ed5e113e7515e223924e5f9e5
william.rocha@bliteti.com.br <william.rocha@bliteti.com.br>
2019-10-31 18:23:16 -0300

    pipes - pipes puro

--- pipes/src/app/app.module.ts
+++ pipes/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
 registerLocaleData(localePt, 'pt');
@@ -7,20 +8,30 @@ registerLocaleData(localePt, 'pt');
 import { AppComponent } from './app.component';
 import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
 import { CamelCasePipe } from './camel-case.pipe';
+import { SettingsService } from './settings.service';
+import { FiltroArrayPipe } from './filtro-array.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     ExemplosPipesComponent,
-    CamelCasePipe
+    CamelCasePipe,
+    FiltroArrayPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [
+    // {
+    //   provide: LOCALE_ID, ==> modo padrão de utilizar o locale e suas configurações 
+    //   useValue: 'pt'
+    // }
+    SettingsService,
     {
-      provide: LOCALE_ID,
-      useValue: 'pt'
+      provide: LOCALE_ID, 
+      deps: [SettingsService],
+      useFactory: (settingsService) => settingsService.getLocale() // ==> modo de padronizar as configurações a partir de um service
     }
   ],
   bootstrap: [AppComponent]
--- pipes/src/app/settings.service.ts
+++ pipes/src/app/settings.service.ts
@@ -6,4 +6,8 @@ import { Injectable } from '@angular/core';
 export class SettingsService {
 
   constructor() { }
+
+  getLocale() {
+    return 'pt';
+  }
 }
