01266481f9b694e242f36a24024e145ef9c8aca9
william.rocha@bliteti.com.br <william.rocha@bliteti.com.br>
2019-10-31 17:29:23 -0300

    pipes

---
+++ pipes/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+registerLocaleData(localePt, 'pt');
+
+import { AppComponent } from './app.component';
+import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
+import { CamelCasePipe } from './camel-case.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ExemplosPipesComponent,
+    CamelCasePipe
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt'
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
