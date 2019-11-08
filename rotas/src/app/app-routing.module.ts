39ffa658a35e1ac2640bae4e8237d5f24d385d5a
CesarWilliam <will.roc@hotmail.com>
2019-11-08 00:18:26 -0300

    rotas

---
+++ rotas/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'cursos', component: CursosComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
