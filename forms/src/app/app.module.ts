94c1ac8b1a2385c55903d2ec7ead5db0dfe1085c
ivan.correa <ivan.correa@i9tec.com.br>
2020-01-09 18:14:57 -0300

    aulas forms

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -1,23 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DataFormComponent } from './data-form/data-form.component';
 import { TemplateFormModule } from './template-form/template-form.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DataFormComponent
+    AppComponent        
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    TemplateFormModule,
+    TemplateFormModule, 
+    ReactiveFormsModule,
     TooltipModule.forRoot()
   ],
   providers: [],
--- forms/src/app/template-form/template-form.module.ts
+++ forms/src/app/template-form/template-form.module.ts
@@ -3,14 +3,20 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { TemplateFormComponent } from './template-form.component';
+import { FormDebugComponent } from '../form-debug/form-debug.component';
+import { CampoControlErroComponent } from '../campo-control-erro/campo-control-erro.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    TemplateFormComponent
+    TemplateFormComponent,
+    FormDebugComponent,
+    CampoControlErroComponent
   ],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ]
 })
 export class TemplateFormModule { }
