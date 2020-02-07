944ae86ec95ae9fb62bc70bb7a3bab8459bbb73d
CesarWilliam <will.roc@hotmail.com>
2020-02-06 22:07:38 -0300

    rotas

--- rotas/src/app/cursos/cursos.component.ts
+++ rotas/src/app/cursos/cursos.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+
 import { CursosService } from './cursos.service';
 
 @Component({
@@ -9,13 +12,30 @@ import { CursosService } from './cursos.service';
 export class CursosComponent implements OnInit {
 
   cursos: any[];
+  pagina: number;
+  inscricao: Subscription
 
   constructor(
-    private cursosService: CursosService
+    private cursosService: CursosService,
+    private route: ActivatedRoute,
+    private router: Router
   ) { }
 
   ngOnInit() {
     this.cursos = this.cursosService.getCursos();
+
+    this.inscricao = this.route.queryParams.subscribe((queryParams: any) => {
+      this.pagina = queryParams['pagina'];
+    }) 
+  }
+
+  ngOnDestroy() {
+    this.inscricao.unsubscribe();
+  }
+
+  proximaPagina() {
+    // this.pagina++;
+    this.router.navigate(['/cursos'], {queryParams: {'pagina': ++this.pagina}});
   }
 
 }
